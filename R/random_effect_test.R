37199e06697711428687d9d6365e127a96334596
Ischi94 <gmathes94@gmx.de>
2021-04-26 11:12:17 +0200

    estimate effect with second random effect

--- R/random_effect_test.R
+++ R/random_effect_test.R
@@ -3,6 +3,8 @@ library(tidyverse)
 library(lme4)
 library(geiger)
 library(here)
+library(brms)
+library(patchwork)
 
 # load self-defined functions 
 source(here("R/functions.R"))
@@ -126,4 +128,174 @@ comparison_warm %>%
 
 
 
+# Make predictions based on GLMM's ----------------------------------------
 
+# make informed predictions based on a subset (palaeoclimate interaction)
+# type = response gives us probability instead of log Odds
+
+#  warming warming
+ww_raw <- subset(dat_final, trend.st8 >=0 & warming >= 0)
+ww_pred <- predict(warm_interaction_final2, newdata = ww_raw,
+                   type = "response")
+
+#  cooling warming
+cw_raw <- subset(dat_final, trend.st8 <=0 & warming >= 0)
+cw_pred <- predict(warm_interaction_final2, newdata = cw_raw,
+                   type = "response")
+
+#  warming cooling 
+wc_raw <- subset(dat_final, trend.st6 >=0 & cooling <= 0)
+wc_pred <- predict(cool_interaction_final2, newdata = wc_raw,
+                   type = "response")
+
+#  cooling cooling 
+cc_raw <- subset(dat_final, trend.st6 <=0 & cooling <= 0)
+cc_pred <- predict(cool_interaction_final2, newdata = cc_raw,
+                   type = "response")
+
+# make a dataframe with the output
+prob_comparison <- tibble(
+  ori.prob = c(cc_pred, wc_pred, cw_pred, ww_pred),
+  pal.int = c(
+    rep("CC", length(cc_pred)),
+    rep("WC", length(wc_pred)),
+    rep("CW", length(cw_pred)),
+    rep("WW", length(ww_pred)))) %>%
+  mutate(
+    # absolute values
+    ori.prob = ori.prob * 100,
+    # combine the rest
+    pal.int = fct_collapse(
+      pal.int,
+      cooling_cooling = "CC",
+      other = c("CW", "WC", "WW")))
+
+
+# calculate average origination response ----------------------------------
+
+# for warm
+odds <- exp(summary(warm_interaction_final2)$coefficients[1])
+prob_warm <- odds / (1 + odds)
+
+# for cool
+odds <- exp(summary(cool_interaction_final2)$coefficients[1])
+prob_cool <- odds / (1 + odds)
+
+# mean
+av <- (prob_cool + prob_warm)/2
+
+
+# bayesian estimation -----------------------------------------------------
+
+# set monte carlo parameters
+CHAINS <- 4
+ITER <- 1000
+WARMUP <- 500
+BAYES_SEED <- 1234
+options(mc.cores = parallel::detectCores())  # Use all cores
+
+# run the model usin brms and rcpp
+brms_best <- brm(
+  # we suppress the intercept by setting ori.prob ~ 0 + pal.int, 
+  # brms returns coefficients for each of the groups, 
+  # and these coefficients represent group means.
+  bf(ori.prob ~ 0 + pal.int, sigma ~ 0 + pal.int), 
+  family = student,
+  data = prob_comparison,
+  prior = c(
+    # Set group mean prior as visualized above
+    # these are fairly informative prior capturing realistic expectations
+    set_prior("normal(15, 30)", class = "b", lb = 0, ub = 100),
+    # Set group variance prior using cauchy(0, 1)
+    set_prior("cauchy(0, 1)", class = "b", dpar = "sigma"),
+    set_prior("exponential(1.0/29)", class = "nu")),
+  chains = CHAINS, iter = ITER, warmup = WARMUP, seed = BAYES_SEED)
+
+
+# extract the posterior samples for each of the groups, subtract them from each other,
+# and then calculate the credible interval
+brms_best_post <- posterior_samples(brms_best) %>% 
+  # Rescale sigmas (sigma terms are on a log scale, 
+  # so we need to exponentiate them back to the scale of the data)
+  mutate_at(vars(contains("sigma")), exp) %>% 
+  # we need to log nu
+  mutate(nu = log10(nu)) %>% 
+  # calculate differences
+  mutate(diff_medians = b_pal.intcooling_cooling - b_pal.intother,
+         diff_sigma = b_sigma_pal.intcooling_cooling - b_sigma_pal.intother)
+
+
+# we can use tidyMCMC from broom to calculate the difference and 
+# create confidence intervals
+brms_best_tidy <- 
+  broom::tidy(brms_best_post, conf.int = TRUE, conf.level = 0.89, 
+              estimate.method = "median", conf.method = "HPDinterval")
+
+# calculate effect
+best_results <- brms_best_tidy %>% 
+  filter(column == "diff_medians") %>% 
+  transmute(estimate = median, conf.low = min, conf.high = max) %>% 
+  # percentage change
+  mutate(across(everything(), 
+                ~./(av*100)*100, 
+                .names = "{col}.perc")) %>% 
+  pivot_longer(cols = everything()) %>% 
+  add_column(type = rep(c("raw", "percentage"), each = 3)) %>% 
+  mutate(name = rep(c("estimate", "conf.low", "conf.high"), 2)) %>% 
+  pivot_wider(names_from = "name") %>% 
+  add_column(random_effect = "two")
+
+
+
+# comparison with actual data ---------------------------------------------
+
+load(here("data/effect_plot_data.RData"))
+
+# bring results in similar format
+actual_results <- effect_plot_data %>% 
+  map_dfr(filter, method == "best") %>%  
+  mutate(type = c("raw", "percentage")) %>% 
+  add_column(random_effect = "one")
+
+# merge data
+raw_diff <- best_results %>% 
+  full_join(actual_results) %>% 
+  # visualize difference in medians
+  filter(type == "raw") %>% 
+  ggplot(aes(estimate, 
+             xmin = conf.low,
+             xmax = conf.high, 
+             random_effect)) +
+  geom_pointrange(size = 1.3, colour = "grey30") +
+  geom_point(size = 3.5, colour = "white") +
+  labs(x = "Difference in medians", 
+       y = NULL) +
+  scale_y_discrete(labels = c("Reported results", "Additional random effect")) +
+  my_theme
+
+  
+
+# merge data
+perc_diff <- best_results %>% 
+  full_join(actual_results) %>% 
+  # visualize difference in medians
+  filter(type == "percentage") %>% 
+  ggplot(aes(estimate, 
+             xmin = conf.low,
+             xmax = conf.high, 
+             random_effect)) +
+  geom_pointrange(size = 1.3, colour = "grey30") +
+  geom_point(size = 3.5, colour = "white") +
+  labs(x = "Percentage change", 
+       y = NULL) +
+  scale_x_continuous(labels = function(x) paste0(x, '%')) +
+  scale_y_discrete(labels = c("Reported results", "Additional random effect")) +
+  my_theme
+
+# combine plots
+combined_plot <- raw_diff / perc_diff +
+  plot_annotation(tag_levels = 'A')
+
+# save it
+ggsave(plot = combined_plot, filename = here("figures/second_effect_test.png"), 
+       units = "cm")
